5a3d3804ef1ff700b4c47f47caf73e332431be6d
Devansh Khandelwal <83805376+devansh-dek@users.noreply.github.com>
2025-01-01 16:26:59 +0530

    Fixed Cors Issue

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -12,10 +12,26 @@ const PORT: number = parseInt(process.env.PORT!);
 const app: Application = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-    origin: '*',
-    credentials: true
-}));
+const allowedOrigins = [
+    'http://localhost:3000', // Local development
+    'https://hg-official-2024-lojc.vercel.app' // Deployed frontend urll :)
+  ];
+  
+  app.use(cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true 
+  }));
+  
+// app.use(cors({
+//     origin: '*',
+//     credentials: true
+// }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.json());
